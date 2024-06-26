1e27c379b94a3876148929fa303728b6df5efa3d
Tim ROBERT <robert.tim.web@gmail.com>
2024-06-26 16:15:40 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ module.exports = env => {
                 ]
             },
             plugins: [
+                // This plugin allows a build to provide or consume modules with other independent builds at runtime.
                 new ModuleFederationPlugin({
                     name: moduleName,
                     library: {type: 'assign', name: `window.appShell = (typeof appShell === "undefined" ? {} : appShell); window.appShell['${moduleName}']`},
@@ -123,6 +124,7 @@ module.exports = env => {
                 ]
             },
             plugins: [
+                // This plugin help you to attach extra files or dirs to webpack's watch system
                 new ExtraWatchWebpackPlugin({
                     files: [
                         'src/server/**/*',
@@ -143,17 +145,19 @@ module.exports = env => {
         }
     ];
 
-    const webpackShellPlugin = new WebpackShellPluginNext({
-        onAfterDone: {
-            scripts: ['yarn jahia-deploy pack']
-        }
-    });
-
     if (env.deploy) {
+        // This plugin allows you to run any shell commands before or after webpack builds.
+        const webpackShellPlugin = new WebpackShellPluginNext({
+            onAfterDone: {
+                scripts: ['yarn jahia-deploy pack']
+            }
+        });
+
         let config = configs[configs.length - 1];
         if (!config.plugins) {
             config.plugins = [];
         }
+
         config.plugins.push(webpackShellPlugin);
     }
 
